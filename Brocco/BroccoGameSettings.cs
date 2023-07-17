e9742009fee4967646b125ef7a6886cdc857b7e4
Raoul1808 <raoulthegeek@gmail.com>
2023-07-17 11:05:49 +0200

    Adjust default resolution according to canvas size

--- Brocco/BroccoGameSettings.cs
+++ Brocco/BroccoGameSettings.cs
@@ -9,27 +9,27 @@ namespace Brocco;
 public struct BroccoGameSettings
 {
     /// <summary>
-    /// The size of the internal canvas in pixels. Not to be confused with the resolution.
+    /// The size of the internal canvas in pixels. Not to be confused with the resolution. Defaults to 640x360.
     /// </summary>
     public Size CanvasSize;
     
     /// <summary>
-    /// Whether the mouse cursor will be shown by default or not.
+    /// Whether the mouse cursor will be shown by default or not. Defaults to false.
     /// </summary>
     public bool ShowMouse;
     
     /// <summary>
-    /// The resolution of the game's window. Not to be confused with the canvas size.
+    /// The resolution of the game's window. Not to be confused with the canvas size. Defaults to 2 times the canvas size.
     /// </summary>
     public Size Resolution;
 
     /// <summary>
-    /// The name of the assets directory from which resources will be loaded.
+    /// The name of the assets directory from which resources will be loaded. Defaults to "Content"
     /// </summary>
     public string AssetsDirectory;
 
     /// <summary>
-    /// The canvas' clear color.
+    /// The canvas' clear color. Defaults to Black.
     /// </summary>
     public Color ClearColor;
 
@@ -40,7 +40,7 @@ public struct BroccoGameSettings
     {
         CanvasSize = new Size(640, 360);
         ShowMouse = false;
-        Resolution = new Size(1280, 720);
+        Resolution = CanvasSize * 2;
         AssetsDirectory = "Content";
         ClearColor = Color.Black;
     }
