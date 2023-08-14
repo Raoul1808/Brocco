955a2a4aabe3b332a0bd8ab6c46bedb922ca72d3
Raoul1808 <raoulthegeek@gmail.com>
2023-08-14 15:22:57 +0200

    What am I even doing

--- Brocco/Util/BroccoMath.cs
+++ Brocco/Util/BroccoMath.cs
@@ -31,7 +31,7 @@ public static class BroccoMath
     /// <param name="p1">First point</param>
     /// <param name="p2">Second point</param>
     /// <returns>The distance between both points as a floating point number.</returns>
-    public static float Distance(this Point value1, Point value2)
+    public static float Distance(Point value1, Point value2)
     {
         float v1 = value1.X - value2.X, v2 = value1.Y - value2.Y;
         return (float) Math.Sqrt((v1 * v1) + (v2 * v2));
