0e892fb8ec59a7385929a94c3323254177923771
Raoul1808 <raoulthegeek@gmail.com>
2023-08-16 14:46:49 +0200

    Clamp is actually useless, not the entire class

--- Brocco/Util/BroccoMath.cs
+++ Brocco/Util/BroccoMath.cs
@@ -6,26 +6,6 @@ namespace Brocco.Util;
 public static class BroccoMath
 {
     /// <summary>
-    /// Restricts a value to be within a specified range.
-    /// </summary>
-    /// <param name="value">The value to clamp.</param>
-    /// <param name="min">
-    /// The minimum value. If <c>value</c> is less than <c>min</c>, <c>min</c>
-    /// will be returned.
-    /// </param>
-    /// <param name="max">
-    /// The maximum value. If <c>value</c> is greater than <c>max</c>, <c>max</c>
-    /// will be returned.
-    /// </param>
-    /// <returns>The clamped value.</returns>
-    public static int Clamp(int value, int min, int max)
-    {
-        value = (value > max) ? max : value;
-        value = (value < min) ? min : value;
-        return value;
-    }
-    
-    /// <summary>
     /// Calculates the distance between two points.
     /// </summary>
     /// <param name="p1">First point</param>
